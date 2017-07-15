c60639a0c60781e50e0639efb17415849a176df5
Patrick <patrick@tayklin.com>
2017-07-15 10:23:27 -0700

    lift summary error redirect working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,15 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <div class="panel-heading">Home</div>
                 <div class="panel-body">
                     You are logged in!
