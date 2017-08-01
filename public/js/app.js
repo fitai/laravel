f92297ad95a063ca4d20d61f09f5a0dfbe6b1aa5
Patrick <patrick@tayklin.com>
2017-08-01 16:15:22 -0700

    lift prefilled fields

--- public/js/app.js
+++ public/js/app.js
@@ -1258,6 +1258,30 @@ var app = new Vue({
                 }
             }
         }.bind(this));
+
+        // Check for test parameter on lift
+        function getUrlParameter(sParam) {
+            var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
+
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : sParameterName[1];
+                }
+            }
+        }
+
+        if (getUrlParameter('test') == 1) {
+            this.liftWeight = 35;
+            this.maxReps = 10;
+            this.$children[0].type = "Deadlift";
+            this.$children[0].variation = "Standard";
+            this.$children[0].equipment = "BB";
+        }
     },
 
     computed: {
