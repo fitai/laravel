75c404bb2b05050fb004c843db7cc9191fd113fd
Patrick <patrick@tayklin.com>
2018-02-02 14:47:50 -0800

    mobile menu click off fixes

--- public/js/global.js
+++ public/js/global.js
@@ -77,19 +77,25 @@ function nl2br(str, is_xhtml) {
 }
 
 // Mobile navigation open listener
-$(document).on('click', '#nav-hamburger', function () {
-  $('#nav-menu').addClass('active').show();
+$(document).on('click', '#nav-hamburger', function (event) {
+  $('#nav-menu').addClass('active');
+  event.stopPropagation();
 });
 
 // Mobile navigation close listener
 $(document).on('click', '#close-menu', function () {
-  $('#nav-menu').removeClass('active').hide();
+  $('#nav-menu').removeClass('active');
 });
 
 // Hide mobile navigation if click off screen
-$(document).on('click', '#dynamic-content', function () {
+// $(document).on('click', '#dynamic-content', function () {
+// 	if ($('#nav-menu').hasClass('active')) {
+// 		$('#nav-menu').removeClass('active').hide();
+// 	}
+// });
+$(document).on('click', '#main', function () {
   if ($('#nav-menu').hasClass('active')) {
-    $('#nav-menu').removeClass('active').hide();
+    $('#nav-menu').removeClass('active');
   }
 });
 
